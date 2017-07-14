e9b765fe69aa842738e2d282e782917a8e3f5a4b
Yehuda Adler <yehuda.adler@gmail.com>
2017-07-14 09:15:31 +0300

    fix white spaces

--- ApiProxy.php
+++ ApiProxy.php
@@ -2,61 +2,61 @@
 
 class ApiProxy {
 
-    const GET_STARSHIPS_URL = "http://swapi.co/api/starships/?format=json";
+	const GET_STARSHIPS_URL = "http://swapi.co/api/starships/?format=json";
 	
-    public function getShips() {
-        $ships = array();
-        $next = self::GET_STARSHIPS_URL;
-          
-        while ($next != null) {
-            
-            $starshipsApiResult = $this->CallAPI("GET", $next);
-            $starshipsApiJson = json_decode($starshipsApiResult, true);
+	public function getShips() {
+		$ships = array();
+		$next = self::GET_STARSHIPS_URL;
+		  
+		while ($next != null) {
+			
+			$starshipsApiResult = $this->CallAPI("GET", $next);
+			$starshipsApiJson = json_decode($starshipsApiResult, true);
 
-            $next = $starshipsApiJson['next'];
+			$next = $starshipsApiJson['next'];
 
-            $ships = array_merge($ships, $starshipsApiJson['results']);
+			$ships = array_merge($ships, $starshipsApiJson['results']);
 
-          }
+		}
 
-        return $ships; 
-    }
+		return $ships; 
+	}
 
 	//https://stackoverflow.com/questions/9802788/call-a-rest-api-in-php
-    private function CallAPI($method, $url, $data = false) {
+	private function CallAPI($method, $url, $data = false) {
 
-        // Method: POST, PUT, GET etc
-        // Data: array("param" => "value") ==> index.php?param=value
+		// Method: POST, PUT, GET etc
+		// Data: array("param" => "value") ==> index.php?param=value
 
-        $curl = curl_init();
+		$curl = curl_init();
 
-        switch ($method)
-        {
-            case "POST":
-                curl_setopt($curl, CURLOPT_POST, 1);
+		switch ($method)
+		{
+			case "POST":
+				curl_setopt($curl, CURLOPT_POST, 1);
 
-                if ($data)
-                    curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-                break;
-            case "PUT":
-                curl_setopt($curl, CURLOPT_PUT, 1);
-                break;
-            default:
-                if ($data)
-                    $url = sprintf("%s?%s", $url, http_build_query($data));
-        }
+				if ($data)
+					curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+				break;
+			case "PUT":
+				curl_setopt($curl, CURLOPT_PUT, 1);
+				break;
+			default:
+				if ($data)
+					$url = sprintf("%s?%s", $url, http_build_query($data));
+		}
 
-        // Optional Authentication:
-        //curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-        //curl_setopt($curl, CURLOPT_USERPWD, "username:password");
+		// Optional Authentication:
+		//curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+		//curl_setopt($curl, CURLOPT_USERPWD, "username:password");
 
-        curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($curl, CURLOPT_URL, $url);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 
-        $result = curl_exec($curl);
+		$result = curl_exec($curl);
 
-        curl_close($curl);
+		curl_close($curl);
 
-        return $result;
-    }
+		return $result;
+	}
 }
--- ShipsFilter.php
+++ ShipsFilter.php
@@ -10,27 +10,27 @@ class ShipsFilter {
 		});
 
 		usort($smallShips, function($elem1, $elem2) {
-	    	return $elem1['cost_in_credits'] <= $elem2['cost_in_credits'];  
-	  	});
+			return $elem1['cost_in_credits'] <= $elem2['cost_in_credits'];  
+		});
 
-	  	return $smallShips;
+		return $smallShips;
 	}
 
 	public function getBigShips($ships) {
 		$bigShips = array_filter($ships, function($elem) {
 			return $elem['crew'] > self::SMALL_SHIPS_MAX_CREW;
-    	});
+		});
 
 		usort($bigShips, function($elem1, $elem2) {
 
 			// Note: if you can turn on intl in php.ini, would be nicer to use numberformatter
-    
+
 			$length1 = (float)str_replace(',', '', $elem1['length']);
 			$length2 = (float)str_replace(',', '', $elem2['length']);
-  
-			return $length1 <= $length2;
-  		});
 
-  		return $bigShips;
+			return $length1 <= $length2;
+		});
+
+		return $bigShips;
 	}
 }
